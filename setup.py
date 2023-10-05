84db9a7dc49162a3190cb7cb89670b878f263c9b
Mahmoud Nasser <mnasserone@gmail.com>
2023-10-05 05:39:52 +0300

    logger and exception added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'boggleBoardsDetection',
+    version= '0.0.0',
+    author= 'Mahhmoud Nasser',
+    author_email= 'nassermanone@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
